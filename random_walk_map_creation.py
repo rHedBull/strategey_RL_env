20fc21ac650ca80be62439c431c897ea35709e15
Hendrik Wirthwein <hendrik.wirthwein@outlook.com>
2023-11-22 21:50:03 +0100

    cleanup, adapted to OOP

--- random_walk_map_creation.py
+++ random_walk_map_creation.py
@@ -1,73 +1,141 @@
 import pygame
-import sys
 import random
 
-# Initialize Pygame
-pygame.init()
+default_land_color = (34, 139, 34)
+default_water_color = (70, 130, 180)
+default_border_color = (0, 0, 0)
 
-# Map dimensions
-width, height = 1270, 1000
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Agent-based Landmass Generation')
+default_land_value = 0
+default_water_value = 1
 
-# Constants
-SQUARE_SIZE = 10  # Size of the squares
-WATER_COLOR =  (34, 139, 34) # Water color
-LAND_COLOR = (70, 130, 180)  # Land color
+default_tile_owner = 0
 
-# Create the world as a 2D array
-world = [[WATER_COLOR for _ in range(width // SQUARE_SIZE)] for _ in range(height // SQUARE_SIZE)]
 
-# Agent class
-class Agent:
-    def __init__(self, x, y, land_budget):
+class MapSquare:
+    def __init__(self, x, y, land_value=default_land_value):
         self.x = x
         self.y = y
-        self.land_budget = land_budget  # Total amount of land to create
+        self.square_size = 10
+        self.default_border_color = default_border_color
+        self.default_color = default_land_color
 
-    def walk(self):
+        self.land_value = land_value
+        self.owner_value = default_tile_owner
+
+    def set_owner(self, owner_value):
+        self.owner_value = owner_value
+
+    def get_owner(self):
+        return self.owner_value
+
+    def set_land_value(self, land_value):
+        self.land_value = land_value
+
+    def get_land_value(self):
+        return self.land_value
+
+    def draw(self, screen):
+        self.draw_square_fill(screen)
+        #self.draw_border(screen)
+
+    def draw_square_fill(self, screen):
+        # change drawn color based on land value
+        color = self.default_color
+        if self.get_land_value() == default_land_value:
+            color = default_land_color
+        else:
+            color = default_water_color
+        pygame.draw.rect(screen, color, (self.x, self.y, self.square_size, self.square_size))
+
+    def draw_border(self, screen):
+        # draw square border
+        color = self.default_border_color
+        if self.get_owner() == default_tile_owner:
+            color = default_border_color
+        else:
+            color = default_water_color
+        pygame.draw.rect(screen, color, (self.x, self.y, self.square_size, self.square_size), 1)
+
+
+class Map:
+    def __init__(self, width=100, height=100):
+        self.width = width
+        self.height = height
+        self.squares = []
+        self.screen = None
+        self.square_size = 10
+
+        self.water_budget_per_agent = 100  # Adjust the total amount of land per agent
+        self.numb_agents = 10
+
+    def create_map(self, width, height):
+        self.width = width
+        self.height = height
+
+        # create map squares
+        self.squares = [[MapSquare(x * self.square_size, y * self.square_size) for x in range(self.width)] for y in
+                        range(self.height)]
+
+        agents = [
+            Map_Agent(random.randint(0, self.width - 1), random.randint(0, self.height - 1),
+                      self.water_budget_per_agent) for i in range(self.numb_agents)]
+
+        # Initialize Pygame
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Agent-based Landmass Generation')
+
+        # Main loop
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # Move each agent and draw the world
+            for agent in agents:
+                agent.walk(self)
+            self.draw(self.screen)
+
+            # Update the display
+            pygame.display.flip()
+
+    def draw(self, screen):
+
+        for row in self.squares:
+            for square in row:
+                square.draw(screen)
+
+    def get_neighbors(self, x, y):
+        # Returns the coordinates of the square's neighbors
+        neighbors = []
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < self.width and 0 <= ny < self.height:
+                neighbors.append((nx, ny))
+        return neighbors
+
+
+# Agent class
+class Map_Agent:
+    def __init__(self, x, y, water_budget):
+        self.x = x
+        self.y = y
+        self.water_budget = water_budget  # Total amount of water to create
+
+    def walk(self, world_map):
         # Random walk step
         step_x = random.choice([-1, 0, 1])
         step_y = random.choice([-1, 0, 1])
         self.x += step_x
         self.y += step_y
         # Keep the agent within bounds of the world
-        self.x = max(0, min(self.x, width // SQUARE_SIZE - 1))
-        self.y = max(0, min(self.y, height // SQUARE_SIZE - 1))
+        self.x = max(0, min(self.x, world_map.width - 1))
+        self.y = max(0, min(self.y, world_map.height - 1))
 
-        # Create land if there is budget left
-        if self.land_budget > 0:
-            if world[self.y][self.x] != LAND_COLOR:
-                world[self.y][self.x] = LAND_COLOR
-                self.land_budget -= 1
-
-# Initialize agents
-num_agents = 10
-land_per_agent = 500  # Adjust the total amount of land per agent
-agents = [Agent(random.randint(0, width // SQUARE_SIZE - 1), random.randint(0, height // SQUARE_SIZE - 1), land_per_agent) for _ in range(num_agents)]
-
-# Function to draw the world
-def draw_world():
-    for y in range(height // SQUARE_SIZE):
-        for x in range(width // SQUARE_SIZE):
-            color = world[y][x]
-            pygame.draw.rect(screen, color, (x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-
-# Main loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Move each agent and draw the world
-    for agent in agents:
-        agent.walk()
-    draw_world()
-
-    # Update the display
-    pygame.display.flip()
-
-# Quit Pygame
-pygame.quit()
-sys.exit()
+        # Create water if there is budget left
+        if self.water_budget > 0:
+            if world_map.squares[self.y][self.x].get_land_value() == default_land_value:
+                world_map.squares[self.y][self.y].set_land_value(default_water_value)
+                self.water_budget -= 1
