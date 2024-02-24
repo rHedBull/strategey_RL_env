b8062e74e9eeecb243a53035879412adc78bcb80
Hendrik Wirthwein <hendrik.wirthwein@outlook.com>
2024-02-25 00:14:31 +0100

    more complex actions manager with action cost checking

--- RL_env/actions.py
+++ RL_env/actions.py
@@ -8,10 +8,11 @@ action_dictionary = {
 
 
 class ActionManager:
-    def __init__(self, env):
+    def __init__(self, env, cost):
         self.env = env
         self.action_to_int = action_dictionary
         self.int_to_action = {v: k for k, v in self.action_to_int.items()}
+        self.action_cost = cost
 
     def get_action_name(self, action_int):
         return self.int_to_action.get(action_int, "Unknown")
@@ -19,8 +20,45 @@ class ActionManager:
     def get_action_int(self, action_name):
         return self.action_to_int.get(action_name, -1)
 
+    def apply_action(self, action, agent, action_properties):
+
+        if action == 'None' or agent.state == 'Done':
+            return
+
+        if not self.check_cost(agent, action):
+            return
+
+        if action == 4:
+            self.claim_tile(agent, action_properties[0], action_properties[1])
+            return
+
+
+        # TODO remove option to move
+        if action == 2:
+            agent.x -= 1
+        elif action == 3:
+            agent.x += 1
+        elif action == 0:
+            agent.y -= 1
+        elif action == 1:
+            agent.y += 1
+
+        agent.x = max(0, min(agent.x, self.env.map.max_x_index - 1))
+        agent.y = max(0, min(agent.y, self.env.map.max_y_index - 1))
+
+    def check_cost(self, agent, action):
+        # if action is move up, down, left, right
+        if action < 4:
+            if agent.money >= self.action_cost.get_setting('move_one'):
+                return True
+
+        elif action == 4:
+            if agent.money >= self.action_cost.get_setting('claim_one_tile'):
+                return True
+
+        return False
+
     def claim_tile(self, agent, x, y):
-        # TODO implement action check by money
         # TODO implement action check map bounds
         # TODO implement claimng of tiles not standing on
 
