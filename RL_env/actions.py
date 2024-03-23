f6e10b432e25837554afedfbcdc2b303bc9d053c
Hendrik Wirthwein <hendrik.wirthwein@outlook.com>
2024-03-23 19:56:14 +0100

    added building feature, untested

--- RL_env/actions.py
+++ RL_env/actions.py
@@ -1,11 +1,19 @@
+from Map.MapSettings import OWNER_DEFAULT_TILE
+
 action_dictionary = {
     'Move Up': 0,
     'Move Down': 1,
     'Move Left': 2,
     'Move Right': 3,
-    'Claim': 4
+    'Claim': 4,
+    'build': 5
 }
 
+buildings = [['improvement-1', 100, 5]]
+
+
+# TODO : one system to manage actions and buildings, preferable over: [ []]
+# TODO : implement graphical building visualization
 
 class ActionManager:
     def __init__(self, env, cost):
@@ -22,39 +30,98 @@ class ActionManager:
 
     def apply_action(self, action, agent, action_properties):
 
-        if action == 'None' or agent.state == 'Done':
+        if action == -1 or agent.state == 'Done':
             return
 
-        if not self.check_cost(agent, action):
+        if not self.check_cost(agent, action, action_properties):
             return
 
         if action == 4:
             self.claim_tile(agent, action_properties[0], action_properties[1])
             return
 
+        if action == 5:  # build something
+            x = action_properties[0]
+            y = action_properties[1]
+            building_id = action_properties[2]
+
         # TODO remove option to move
         self.move_agent(agent, action)
 
-    def check_cost(self, agent, action):
+    def check_cost(self, agent, action, action_properties):
         # if action is move up, down, left, right
+
         if action < 4:
+            x = agent.x
+            y = agent.y
+
             if agent.money >= self.action_cost.get_setting('move_one'):
-                return True
+
+                # check for map bounds
+                if x < 0 or x > self.env.map.max_x_index - 1 or y < 0 or y > self.env.map.max_y_index - 1:
+                    return False
+                else:
+                    return True
 
         elif action == 4:
-            if agent.money >= self.action_cost.get_setting('claim_one_tile'):
+
+            claim_cost = self.action_cost.get_setting('claim_one_tile')
+
+            #check if the tile is already claimed by someone
+            x = action_properties[0]
+            y = action_properties[1]
+            tile_claim_id = tile_build = y * self.env.map.max_x_index + x
+
+            if self.env.map.squares[x][y].get_owner() != OWNER_DEFAULT_TILE:
+                return False
+                # TODO add some conflict hanlding here or just increased tile purchase cost
+
+
+            # check if enough money to claim tile
+            if agent.money >= claim_cost:
                 return True
+
+        elif action == 5:
+            # check if building is possible
+            x = action_properties[0]
+            y = action_properties[1]
+            building_id = action_properties[2]
+            building_cost = buildings[building_id]
+            current_buildings_on_tile = self.env.map.get_tile(x, y).buildings
+
+            # check if building is possible
+
+            # check if agent owns the tile to build on
+            claimed_tiles = agent.claimed_tiles
+            tile_build = y * self.env.map.max_x_index + x
+
+            tile_owned = False
+            for tile in claimed_tiles:
+                if tile == tile_build:
+                    tile_owned = True
+                    continue
+
+            if not tile_owned:
+                return False
+
+            # check if building already exists
+            for building in current_buildings_on_tile:
+                if building[0] == building_id:
+                    return False
+            if building_cost <= agent.money:
+                return True
+            else:
+                return False
 
         return False
 
     def claim_tile(self, agent, x, y):
-        # TODO implement action check map bounds
-
-        if x < 0 or x > self.env.map.max_x_index - 1 or y < 0 or y > self.env.map.max_y_index - 1:
-            return
 
         self.env.map.claim_tile(agent, x, y)
         agent.claimed_tiles.append(self.env.map.get_tile(x, y))
+
+    def add_building(self, building_id, x, y):
+        self.env.map.add_building(building_id, x, y)
 
     def move_agent(self, agent, action):
 
