3547a064d6b933059f00544f4503ac0d9d752c09
Hendrik Wirthwein <hendrik.wirthwein@outlook.com>
2024-10-25 10:34:57 +0200

    city setup

---
+++ rl_env/city.py
@@ -0,0 +1,16 @@
+from uuid import uuid1
+
+from typing import Tuple
+
+
+class city:
+
+    def __init__(self, agent_id: int, position: Tuple[int, int]):
+
+        self.id = uuid1()
+
+        self.x, self.y  = position
+
+        self.level = 0
+        self.owner = agent_id # can change in theory
+
