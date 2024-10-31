e85e6ba3a0e0088f06e7e8dc6dd0a5b86a35c2f3
Hendrik Wirthwein <hendrik.wirthwein@outlook.com>
2024-10-31 16:24:12 +0100

    Ownable class

---
+++ rl_env/objects/Ownable.py
@@ -0,0 +1,21 @@
+
+class Ownable:
+    """
+    Mixin class to add ownership capabilities to buildings.
+    """
+
+    def __init__(self, agent_id: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.owner = agent_id  # ID of the owning agent
+
+    def set_owner(self, agent_id: int):
+        """
+        Assign ownership to an agent.
+        """
+        self.owner = agent_id
+
+    def get_owner(self) -> int:
+        """
+        Get the ID of the owning agent.
+        """
+        return self.owner
