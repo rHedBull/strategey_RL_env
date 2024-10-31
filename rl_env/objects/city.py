37851d4bf2fc4b4264db8eba1e5ab54a09d07606
Hendrik Wirthwein <hendrik.wirthwein@outlook.com>
2024-10-31 16:25:04 +0100

    City class as child of Ownable and Building

--- rl_env/objects/city.py
+++ rl_env/objects/city.py
@@ -1,12 +1,34 @@
 from typing import Tuple
 
-from rl_env.objects.MapObject import MapObject
+import pygame
+
+from rl_env.objects.Building import Building, BuildingType
+from rl_env.objects.Ownable import Ownable
 
 
-class City(MapObject):
+class City(Building, Ownable):
 
-    def __init__(self, agent_id: int, position: Tuple[int, int]):
-        super().__init__(position)
+    def __init__(self, agent_id: int, position: Tuple[int, int], image: pygame.Surface):
 
-        self.owner = agent_id # can change in theory
+        Ownable.__init__(self, agent_id)
+        Building.__init__(self, position)
+        #self.image = pygame.transform.scale(image, (image.get_width(), image.get_height()))
+
         self.max_level = 3
+        self.building_type = BuildingType.CITY
+
+    def draw(self, screen: pygame.Surface, square_size: int, owner_color: Tuple[int, int, int]):
+        # Draw the city image centered on the tile
+        pygame.draw.rect(
+            screen,
+            owner_color,
+            (
+                self.x * square_size,
+                self.y * square_size,
+                square_size,
+                square_size,
+            ),
+        )
+
+    def get_building_type(self) -> str:
+        return "City"
