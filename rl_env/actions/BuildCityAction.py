80ffbfdc606369a32889322a40fe4d62510eb586
Hendrik Wirthwein <hendrik.wirthwein@outlook.com>
2024-11-02 09:35:32 +0100

    remove special get_cost and get_reward and build_type methods

--- rl_env/actions/BuildCityAction.py
+++ rl_env/actions/BuildCityAction.py
@@ -8,22 +8,13 @@ from rl_env.objects.city import City
 
 class BuildCityAction(BuildAction):
     def __init__(self, agent: Agent, position: Tuple[int, int]):
-        super().__init__(agent, position)
-
-    def get_cost(self, env) -> float:
-        return env.env_settings.get_setting("actions")["city"]["cost"]
-
-    def get_reward(self, env) -> float:
-        return env.env_settings.get_setting("actions")["city"]["reward"]
+        super().__init__(agent, position, BuildingType.CITY)
 
     def perform_build(self, env):
-        city = City(self.agent.id, self.build_position, None)
-        tile = env.map.get_tile(self.build_position)
+        city = City(self.agent.id, self.position)
+        tile = env.map.get_tile(self.position)
         tile.add_building(city)
         # TODO: add city to agent's list of buildings
-        env.map.claim_tile(self.agent, self.build_position)
-        self.agent.claimed_tiles.add(self.build_position)
-        env.action_manager.update_claimable_tiles(self.agent, self.build_position)
-
-    def build_type(self) -> BuildingType:
-        return BuildingType.CITY
+        env.map.claim_tile(self.agent, self.position)
+        self.agent.claimed_tiles.add(self.position)
+        env.action_manager.update_claimable_tiles(self.agent, self.position)
