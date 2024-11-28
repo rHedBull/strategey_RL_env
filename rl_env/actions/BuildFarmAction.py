29ded2f2bfa313ccfd1c262e6ac4853388ae2412
Hendrik Wirthwein <hendrik.wirthwein@outlook.com>
2024-11-28 13:19:51 +0100

    additional validation step if tile already self claimed

--- rl_env/actions/BuildFarmAction.py
+++ rl_env/actions/BuildFarmAction.py
@@ -10,6 +10,22 @@ class BuildFarmAction(BuildAction):
     def __init__(self, agent: Agent, position: Tuple[int, int]):
         super().__init__(agent, position, BuildingType.FARM)
 
+    def validate(self, env) -> bool:
+        if not super().validate(env):
+            return False
+
+        tile = env.map.get_tile(self.position)
+        tile_owner_id = tile.get_owner()
+
+        if tile_owner_id == self.agent.id:
+            if tile.has_any_building():
+                print(f"Tile{self.position} already has a Building.")
+                return False
+            else:
+                return True
+
+        return False
+
     def perform_build(self, env):
         building_type_id = self.get_building_type_id(env)
         farm = Farm(self.agent.id, self.position, building_type_id)
